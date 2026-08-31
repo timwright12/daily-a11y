b4605cd49efd062a14a75550a90183cc92d00fdd
Tim Wright <timwright12@gmail.com>
2026-08-31 10:27:54 -0400

    refactor: extract shared renderMasthead helper from duplicated entry-point headers

--- src/random.js
+++ src/random.js
@@ -1,6 +1,10 @@
 import { loadCriteria, sortCriteria } from "./content/loadCriteria.js";
 import { randomIndex } from "./rotation.js";
-import { renderCriterionContent, wireCheckFeedback } from "./render.js";
+import {
+  renderCriterionContent,
+  wireCheckFeedback,
+  renderMasthead,
+} from "./render.js";
 import "prismjs/themes/prism.css";
 import "prismjs/components/prism-css.js";
 import "prismjs/components/prism-javascript.js";
@@ -15,15 +19,8 @@ const criteria = sortCriteria(loadCriteria(rawEntries));
 const criterion = criteria[randomIndex(criteria.length)];
 
 const app = document.getElementById("app");
-app.innerHTML = `
-  <header>
-    <div class="masthead">
-      <span class="masthead-mark">Daily Accessibility</span>
-      <span class="admin-label">Random criterion</span>
-    </div>
-  </header>
-  <main></main>
-`;
+renderMasthead(app, "Random criterion");
+app.insertAdjacentHTML("beforeend", "<main></main>");
 
 const main = document.querySelector("main");
 renderCriterionContent(main, criterion);
